42969624aef388ea95e6000c174de2554c91e86d
Natália Oliveira <nataliaferreiraoliveira1994@gmail.com>
2025-02-11 09:31:28 -0300

    feat: add post users route

--- routes/routes.go
+++ routes/routes.go
@@ -3,11 +3,13 @@ package routes
 import "github.com/gin-gonic/gin"
 
 func RegisterRoutes(server *gin.Engine) {
+	server.GET("/healthcheck", healthcheck)
+
 	server.GET("/events", getEvents)
 	server.GET("/events/:id", getEvent)
 	server.POST("/events", createEvent)
 	server.PUT("/events/:id", updateEvent)
 	server.DELETE("/events/:id", deleteEvent)
 
-	server.GET("/healthcheck", healthcheck)
+	server.POST("/users", createUser)
 }
