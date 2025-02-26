23d58f461b6dda5b850d940c7d2cd1477fedc1dc
Natália Oliveira <nataliaferreiraoliveira1994@gmail.com>
2025-02-26 18:56:31 -0300

    feat: add jwt token verification

--- utils/jwt.go
+++ utils/jwt.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"errors"
 	"time"
 
 	"github.com/golang-jwt/jwt"
@@ -16,4 +17,36 @@ func GenerateToken(email string, userId int64) (string, error) {
 	})
 
 	return token.SignedString([]byte(dummykey))
+}
+
+func VerifyToken(token string) (int64, error) {
+	parsedToken, err := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
+		// Check type
+		_, ok := token.Method.(*jwt.SigningMethodHMAC)
+		if !ok {
+			return nil, errors.New("not authorized")
+		}
+
+		return []byte(dummykey), nil
+	})
+	if err != nil {
+		return 0, errors.New("not authorized")
+	}
+
+	tokenIsValid := parsedToken.Valid
+	if !tokenIsValid {
+		return 0, errors.New("not authorized")
+	}
+
+	// Check type
+	claims, ok := parsedToken.Claims.(jwt.MapClaims)
+	if !ok {
+		return 0, errors.New("not authorized")
+	}
+
+	// The int64 is read into float64
+	// https://github.com/dgrijalva/jwt-go/issues/287
+	userId := int64(claims["userId"].(float64))
+
+	return userId, nil
 }
